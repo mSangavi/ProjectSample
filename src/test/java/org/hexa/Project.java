8267dab2c5ad92c8e168cf7bed92859916d1ee06
mSangavi <sangavimsagi@gmail.com>
2022-09-09 11:40:15 +0530

    added new methods in maven branch

--- src/test/java/org/hexa/Project.java
+++ src/test/java/org/hexa/Project.java
@@ -7,4 +7,10 @@ public class Project {
  private void method2() {
     System.out.println("method 2");
 }
+ private void method5() {
+	    System.out.println("method 5");
+	}
+ private void method6() {
+	    System.out.println("method 6");
+	}
 }
